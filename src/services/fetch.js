9d67fb11cab3b5df41e8dd51a6e1eec9ffe07959
Божидар Димов <bojko958@abv.bg>
2018-07-21 18:13:54 +0300

    initial commit

---
+++ src/services/fetch.js
@@ -0,0 +1,68 @@
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../clientConfig.js')[env];
+
+console.log(env);
+console.log(config);
+
+module.exports = {
+  post: function (path, data, onSuccess, onError) {
+    let options = {
+      body: JSON.stringify(data), // must match 'Content-Type' header
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+    };
+
+    let tokenInfo = JSON.parse(window.localStorage.getItem('token'));
+    if (tokenInfo)
+      options.headers['Authorization'] = 'Bearer ' + tokenInfo.token;
+
+    return fetch(config.backend + path, options)
+      .then(res => {
+        message_handler(onError);
+        return error_handler(res)
+      })
+      .then(res => {
+        message_handler(onSuccess);
+        return res.json()
+      });
+
+  },
+  get: function (path) {
+    let options = {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    let tokenInfo = JSON.parse(window.localStorage.getItem('token'));
+    if (tokenInfo)
+      options.headers['Authorization'] = 'Bearer ' + tokenInfo.token;
+
+    return fetch(config.backend + path, options).then(res => error_handler(res))
+      .then(res => res.json());
+  }
+}
+
+function error_handler(res) {
+  if (res.status === 422)
+    return res.json().then(err => { throw err; });
+  if (res.status === 500)
+    return res.json().then(err => { throw err });
+  if (res.status === 401) {
+    window.localStorage.setItem('token', null);
+    window.location.replace('login');
+  }
+  else return res;
+}
+
+function message_handler(message) {
+  let el = document.getElementById("messages");
+  let ev = new CustomEvent('message', { detail: message });
+  if (message)
+    el.dispatchEvent(ev);
+}
