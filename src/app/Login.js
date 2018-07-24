6caab5cd24724ad96d44473959615efdabf2e22d
Божидар Димов <bojko958@abv.bg>
2018-07-24 14:46:12 +0300

    added validation error for login

--- src/app/Login.js
+++ src/app/Login.js
@@ -4,14 +4,26 @@ import * as UserService from '../services/user';
 import { RedirectAction } from '../components';
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {}
+    }
+  }
+
   login() {
     return post('/login', this.state)
+      .catch(err => {
+        this.setState({ errors: err })
+        throw null;
+      })
       .then(token => {
+        debugger;
         let user = token.user;
         token.user = undefined;
         UserService.login(token, user);
         return Promise.resolve();
-      });
+      })
   }
 
   render() {
@@ -28,6 +40,9 @@ export default class Login extends React.Component {
             <div className="col-12">
               <input name="password" placeholder="Парола" type="password" onChange={e => this.setState({ password: e.target.value })} />
             </div>
+            {this.state.errors.login ? <div className="error">
+              *Неправилно потребителско име/парола
+        </div> : null}
           </div>
         </form>
         <ul className="actions">
