01deb74fc864049435ebe7a4f93c3bd932889a79
Божидар Димов <bojko958@abv.bg>
2018-08-14 16:39:55 +0300

    recovery styles

--- src/app/Recovery.js
+++ src/app/Recovery.js
@@ -38,9 +38,7 @@ export default class Recovery extends React.Component {
     post(`/users/recovery`, this.state)
       .then(() => this.setState({ showMessage: true }))
       .catch(err => this.setState({
-        errors: {
-          invalidToken: true
-        }
+        errors: err
       }));
   }
 
@@ -53,7 +51,7 @@ export default class Recovery extends React.Component {
               <header>
                 <h2>Възстановяване на парола</h2>
               </header>
-              <form style={{ display: 'flex', justifyContent: 'center' }} onSubmit={(e) => e.preventDefault()}>
+              <form style={{ display: 'flex', justifyContent: 'center' }} onSubmit={(e) => { e.preventDefault(); this.acceptPasswordRecovery() }}>
                 <div className="row gtr-50" style={{ maxWidth: '35rem' }}>
                   <div className="col-12">
                     Моля въведете новата си парола. Като допълнителна мярка за защита ви молим да въведете и датата си на раждане.
@@ -61,6 +59,7 @@ export default class Recovery extends React.Component {
                   <div className="col-12">
                     <input placeholder="Парола" type="password"
                       onChange={e => this.setState({ password: e.target.value })} />
+                    <div className="error">{this.state.errors.password ? '*Паролата трябва да бъде поне 8 символа, да съдържа поне една цифра, една малка буква и една голяма буква' : null}</div>
                   </div>
                   <div className="col-12">
                     <input placeholder="Повтори парола" type="password"
@@ -70,24 +69,25 @@ export default class Recovery extends React.Component {
                   <div className="col-12">
                     <input placeholder="Дата на раждане" type="date"
                       onChange={e => this.setState({ birthDate: e.target.value })} />
+                    <div className="error">{this.state.errors.birthDate ? 'Неправилна дата на раждане' : null}</div>
                     <div className="error" style={{ textAlign: 'center' }}>{this.state.errors.invalidToken ? 'Линкът за възстановяване на парола е невалиден' : null}</div>
                   </div>
                 </div>
               </form>
               {this.state.showMessage ?
                 <React.Fragment>
+                  <ul className="actions">
+                    <li><div className="login-button" onClick={() => window.location = '/login'}>Вход</div></li>
+                  </ul>
                   <div style={{ color: '#84DC00', fontWeight: '700' }}>
                     Паролата ви е променена успешно
-                </div>
-                  <ul className="actions">
-                    <li><Link to="/login" className="button">Вход</Link></li>
-                  </ul>
+                  </div>
                 </React.Fragment> :
                 <ul className="actions">
                   <li>
-                    <a className="button" onClick={() => this.acceptPasswordRecovery()}>
+                    <div className="login-button" onClick={() => this.acceptPasswordRecovery()}>
                       Запазване
-                  </a>
+                  </div>
                   </li>
                 </ul>}
             </section>
@@ -101,7 +101,7 @@ export default class Recovery extends React.Component {
             <header>
               <h2>Възстановяване на парола</h2>
             </header>
-            <form style={{ display: 'flex', justifyContent: 'center' }} onSubmit={(e) => e.preventDefault()}>
+            <form style={{ display: 'flex', justifyContent: 'center' }} onSubmit={(e) => { e.preventDefault(); this.sendPasswordRecovery() }}>
               <div className="row gtr-50" style={{ maxWidth: '35rem' }}>
                 <div className="col-12">
                   Моля въведете Вашият имейл. Ще ви бъде изпратено съобщение с инструкции как да възстановите паролата си
@@ -115,9 +115,9 @@ export default class Recovery extends React.Component {
             </form>
             <ul className="actions">
               <li>
-                <a className="button" onClick={() => this.sendPasswordRecovery()}>
+                <div className="login-button" onClick={() => this.sendPasswordRecovery()}>
                   Изпращане
-            </a>
+            </div>
               </li>
             </ul>
             {this.state.showMessage ?
