b48f8bc71f3ecf76d13eb37bc00bf501b3ece069
Божидар Димов <bojko958@abv.bg>
2018-08-24 21:37:18 +0300

    proxying request to backend

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const proxy = require('http-proxy').createProxyServer({});
 
 app.listen(80, () => console.log('SmileCup static files on port 80.'));
 app.use(cors());
 
 app.use('/', express.static('build'));
+
+app.use('/api/payments', (req, res, next) => {
+  console.log('caught request\n', req);
+  proxy.web(req, res, { target: 'http://localhost:8080/api/payments' }, next);
+});
 app.get('*', (req, res) => {
   res.sendFile('build/index.html', { root: './' });
 });
